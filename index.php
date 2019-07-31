0035782ae776c578ac0995f088a2178201792850
desiree182000 <53499701+desiree182000@users.noreply.github.com>
2019-07-31 09:34:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 <html>
 <head><title>Profile Page</title></head>
 <body>
-  Hello World!
+  <main>
+    <article>
+      <header>
+        <h1 align="center">Desiree P. Armojallas</h1>
+        <h3 align="center">BSIT-1</h3>
+      </header>
+    </article>
+  </main>
 </body>
 </head>
 </html>
